761cfdd26a9370a61bbcf79fd1518df54c2fb288
anuragshaw-Pelocal <anurag.shaw@pelocal.com>
2026-05-11 10:31:25 +0530

    Login complete need to check

--- src/app.js
+++ src/app.js
@@ -2,8 +2,14 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
 const http = require("http");
+
+require("dotenv").config();
+app.use(express.json());
+app.use(cookieParser());
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 require("dotenv").config();
 app.use(express.json());
@@ -13,9 +19,15 @@ const authRouter = require("./routes/auth");
 
 app.use("/", authRouter);
 
-connectDB();
+const server = http.createServer(app);
 
-
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000...");
-});
+connectDB()
+  .then(()=>{
+    console.log("Connected to database successfully...");
+      server.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+      });
+  })
+  .catch((err)=>{
+    console.error("Failed to connect to database:", err);
+  });
